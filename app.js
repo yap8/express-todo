4471191c3cea1dc8dc7816bd64dc740b206dd966
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-01-02 03:34:33 +0300

    bug fix

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app
-  .get('/', require('./routes/index'))
-  .post('/', require('./routes/index'))
-  .post('/:id', require('./routes/index'))
+app.use('/', require('./routes/index'))
 
 app.listen(3000, () => {
   console.log('------------------')
